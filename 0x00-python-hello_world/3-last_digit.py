d7d1d93e89f398b66c6bfceb44ace92ee65a3aba
clybowl <4941@holbertonschool.com>
2022-08-30 12:01:48 -0700

    task 3-last

--- 0x00-python-hello_world/3-last_digit.py
+++ 0x00-python-hello_world/3-last_digit.py
@@ -2,13 +2,13 @@
 import random
 number = random.randint(-10000, 10000)
 if number < 0:
-	lastdig = number % -10
+    lastdig = number % -10
 else:
-	lastdig = number % 10
+    lastdig = number % 10
 print("Last digit of {:d} is {:d} ".format(number, lastdig), end="")
 if lastdig > 5:
-	print('and is greater than 5')
+    print('and is greater than 5')
 if lastdig == 0:
-	print('and is 0')
+    print('and is 0')
 if lastdig < 6 and lastdig != 0:
-	print('and is less than 6 and not 0')
+    print('and is less than 6 and not 0')
