11e5646f7a88eb296dd02a7141026207b739656a
Clayton Christian <103296303+claybowl@users.noreply.github.com>
2022-08-31 16:16:14 -0500

    format update

--- 0x01-python-import_modules/1-args.py
+++ 0x01-python-import_modules/1-args.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-	num = len(sys.argv)
+    num = len(sys.argv)
     if num == 1:
         print("{} arguments:,".format(num - 1))
     elif num == 2:
