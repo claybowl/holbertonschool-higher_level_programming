8354e4ddec87e9692ec5381511379f50a0b2433f
Clayton Christian <103296303+claybowl@users.noreply.github.com>
2022-08-31 14:23:21 -0500

    format changes

--- 0x01-python-import_modules/1-args.py
+++ 0x01-python-import_modules/1-args.py
@@ -6,11 +6,11 @@ if __name__ == "__main__":
     import sys
 	numb = len(sys.argv)
     if numb == 1:
-        print("{} arguments,".format(numb - 1))
+        print("{} arguments:,".format(numb - 1))
     elif numb == 2:
-        print("{} arguments,".format(numb - 1))
+        print("{} arguments:,".format(numb - 1))
     else:
-        print("{} arguments,".format(numb - 1))
+        print("{} arguments:,".format(numb - 1))
 
     for i in range(1, numb):
         print("{}: {}".format(i, sys.argv[i]))
