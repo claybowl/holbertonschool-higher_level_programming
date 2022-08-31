d41e1505c956fe9f6a846c160d1e67b3108dbb8a
Clayton Christian <103296303+claybowl@users.noreply.github.com>
2022-08-31 16:23:44 -0500

    task 5 update

--- 0x01-python-import_modules/1-args.py
+++ 0x01-python-import_modules/1-args.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
+import sys
+
 if __name__ == "__main__":
-    import sys
     num = len(sys.argv)
     if num == 1:
-        print("{} arguments:,".format(num - 1))
+        print("{0 arguments.")
     elif num == 2:
-        print("{} arguments:,".format(num - 1))
+        print("1 argument:")
+        print("1: {:s}".format(sys.argv[1]))
     else:
-        print("{} arguments:,".format(num - 1))
-
+        print("{:d} arguments:".format(num - 1))
     for i in range(1, num):
-        print("{}: {}".format(i, sys.argv[i]))
+        print("{:d}: {:s}".format(i, sys.argv[i]))
