c856f017da007152c90679999b050a90a1903c71
Clayton Christian <103296303+claybowl@users.noreply.github.com>
2022-09-01 00:13:16 -0500

    attempt task 2

--- 0x01-python-import_modules/1-args.py
+++ 0x01-python-import_modules/1-args.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 import sys
 
-   if (len(sys.argv) - 1) == 0:
+if __name__ == '__main__':
+
+    if (len(sys.argv) - 1) == 0:
         print("0 arguments.")
     else:
-        if (len(sys.argv) - 1) == 1:
-            print((len(sys.argv) - 1), "argument:")
-        else:
-            print((len(sys.argv) - 1), "arguments:")
-        for i in range(1, len(sys.argv)):
-            print("{}: {}".format(i, sys.argv[i]))
+            if (len(sys.argv) - 1) == 1:
+                print((len(sys.argv) - 1), "argument:")
+            else:
+                print((len(sys.argv) - 1), "arguments:")
+            for i in range(1, len(sys.argv)):
+                print("{}: {}".format(i, sys.argv[i]))
