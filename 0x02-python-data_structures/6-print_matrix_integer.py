6971809f80b3b4744214d5c6d6ff62ad8faf1acd
Clayton Christian <103296303+claybowl@users.noreply.github.com>
2022-09-01 22:52:39 -0500

    task 6 attempt

--- 0x02-python-data_structures/6-print_matrix_integer.py
+++ 0x02-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for col in row:
-            print("{:d}".format(col), end=" " if col != row[-1] else "")
-    print()
+    for ro in range(len(matrix)):
+        for co in range(len(matrix[ro])):
+            print("{:d}".format(matrix[ro][co]), end='')
+            if co < len(matrix[ro]) - 1:
+                print(" ", end='')
+        print("".format('\n'))
