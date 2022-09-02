acbaae1c879de5b399d76425a2afa0bac1912660
Clayton Christian <103296303+claybowl@users.noreply.github.com>
2022-09-01 22:47:49 -0500

    task 2 attempt

--- 0x02-python-data_structures/2-replace_in_list.py
+++ 0x02-python-data_structures/2-replace_in_list.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    if idx < 0 or idx > len(my_list) - 1:
+    listlen = len(my_list)
+    if idx < 0 or idx >= listlen:
         return my_list
-    else:
-        my_list[idx] = element
-        return my_list
+    for i in range(listlen):
+        if i == idx:
+            my_list[i] = element
+            return my_list
