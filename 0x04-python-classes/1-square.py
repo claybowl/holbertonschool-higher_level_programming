dc9cb1820089dd9e6911ca0be0f625f0232add75
clybowl <4941@holbertonschool.com>
2022-09-08 11:21:09 -0700

    task 1

--- 0x04-python-classes/1-square.py
+++ 0x04-python-classes/1-square.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 
 from asyncio.constants import SENDFILE_FALLBACK_READBUFFER_SIZE
-
+"""Asyncio"""
 
 class Square:
+    """ class for defined Square """
     def __init__(self, size=0):
-        if type(size) != int:
+            """__init__ defined"""
+	    if type(size) != int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
