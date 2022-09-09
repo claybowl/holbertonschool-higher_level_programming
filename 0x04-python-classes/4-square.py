97ac180890c61ef848ad4388af74b23d149291ac
Clayton Christian <103296303+claybowl@users.noreply.github.com>
2022-09-08 22:00:57 -0500

    task 2,3,4 attempt

--- 0x04-python-classes/4-square.py
+++ 0x04-python-classes/4-square.py
@@ -10,4 +10,30 @@ class Square:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        else:
+            self.__size = size
+
+    def area(self):
+        return (self.__size ** 2)
+
+    @property
+    def size(self):  # method to retrieve squares size
+        return self.__size
+
+    @size.setter
+    def size(self, value):  # method to set/change squares size
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def my_print(self):
+        """prints square to stdout w/ character #"""
+        if self.__size == 0:
+            print("")
+        for row in range(self.__size):
+            for elem in range(self.__size):
+                print("#", end="")
+            print("")
