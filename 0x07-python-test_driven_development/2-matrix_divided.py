b47e94cd8b1d6045d64e6e2838e5fb467b3ea0e8
Clayton Christian <103296303+claybowl@users.noreply.github.com>
2022-09-12 18:15:51 -0500

    task 1

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,28 +1,25 @@
 #!/usr/bin/python3
-"""
-function that divides all elements of a matrix
-
-"""
+""" divide a matrix by div"""
 
 
 def matrix_divided(matrix, div):
-    """
-    this function divides all elements of matrix
-    """
+    """ much error checking, then maths"""
 
-    if not isinstance(matrix, list):
-        raise TypeError("matrix must be a matrix (list of lists) \
-                        of integers/floats")
+    for row in matrix:
+        for val in row:
+            val_type = type(val)
+            if val_type is not int and val_type is not float:
+                raise TypeError("matrix must be a matrix (list of lists) \
+of integers/floats")
+
+    matrix_row_size = len(matrix[0])
+    for row in matrix:
+        if len(row) != matrix_row_size:
+            raise TypeError("Each row of the matrix must have the same size")
 
     div_type = type(div)
     if div_type is not int and div_type is not float:
         raise TypeError("div must be a number")
-
-    len_rows = []
-    for row in matrix:
-        len_rows.append(len(row))
-    if not all(elem == len_rows[0] for elem in len_rows):
-        raise TypeError("Each row of the matrix must have the same size")
 
     if div == 0:
         raise ZeroDivisionError("division by zero")
@@ -30,5 +27,4 @@ def matrix_divided(matrix, div):
     new_matrix = []
     for row in matrix:
         new_matrix.append([round(val/div, 2) for val in row])
-
     return new_matrix
