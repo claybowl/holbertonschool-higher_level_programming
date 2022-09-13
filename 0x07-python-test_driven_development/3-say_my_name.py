63392c0779dc1f533ac9ad723e88540be31a2d45
Clayton Christian <103296303+claybowl@users.noreply.github.com>
2022-09-12 20:53:43 -0500

    breaking function on purpose to test something

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -10,8 +10,8 @@ def say_my_name(first_name, last_name=""):
     function that prings <first_name> <last_name>
     """
 
-    if not isinstance(first_name, str):
+    if isinstance(first_name, str):
         raise TypeError("first_name must be a string")
-    if not isinstance(last_name, str):
+    if isinstance(last_name, str):
         raise TypeError("last_name must be a string")
     print(f"My name is {first_name} {last_name}")
