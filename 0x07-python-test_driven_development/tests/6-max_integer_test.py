0d33522a7c21a5686210be7cce5aba9f78a21bea
Clayton Christian <103296303+claybowl@users.noreply.github.com>
2022-09-12 23:52:33 -0500

    added comments

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -10,17 +10,15 @@ max_integer = __import__('6-max_integer').max_integer
 class TestMaxInteger(unittest.TestCase):
     """TestCase for the max_integer function."""
 
-def single_int_test(self):
-    v1 = [36]
-    self.assertEqual(max_integer(v1), 36)
-
 def multi_int_test(self):
-	v1 = [2, 3, 5, 8, 13, 21, 34]
-	test = max_integer(v1)
-	self.assertEqual(test, 34)
+    """test multi integer list"""
+    v1 = [2, 3, 5, 8, 13, 21, 34]
+    test = max_integer(v1)
+    self.assertEqual(test, 34)
 
 def not_int_test(self):
-    v1 = [2, 3, 5, 8, 13, 21, 34]
+    """testing for non integer"""
+    v1 = [2, 3, 5, "hi", 13, 21, 34]
     test = max_integer(v1)
     self.assertEqual(test, 34)
 
