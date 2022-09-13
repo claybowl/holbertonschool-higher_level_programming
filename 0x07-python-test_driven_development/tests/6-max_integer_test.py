d1f24f3f6646e19197ad3915ae0d16246b307723
Clayton Christian <103296303+claybowl@users.noreply.github.com>
2022-09-12 23:48:20 -0500

    test design

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,18 +1,28 @@
 #!/usr/bin/python3
-"""Module to find the max integer in a list
+"""Unittest for max_integer([..])
 """
 
+import unittest
+max_integer = __import__('6-max_integer').max_integer
 
-def max_integer(list=[]):
-    """Function to find and return the max integer in a list of integers
-        If the list is empty, the function returns None
-    """
-    if len(list) == 0:
-        return None
-    result = list[0]
-    i = 1
-    while i < len(list):
-        if list[i] > result:
-            result = list[i]
-        i += 1
-    return result
+
+class TestMaxInteger(unittest.TestCase):
+    """TestCase for the max_integer function."""
+
+def single_int_test(self):
+    v1 = [36]
+    self.assertEqual(max_integer(v1), 36)
+
+def multi_int_test(self):
+	v1 = [2, 3, 5, 8, 13, 21, 34]
+	test = max_integer(v1)
+	self.assertEqual(test, 34)
+
+def not_int_test(self):
+    v1 = [2, 3, 5, 8, 13, 21, 34]
+    test = max_integer(v1)
+    self.assertEqual(test, 34)
+
+
+if __name__ == '__main__':
+    unittest.main()
