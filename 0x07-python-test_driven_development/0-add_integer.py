2e825d44afe245df26fc1bfec18dea4039714f5c
Clayton Christian <103296303+claybowl@users.noreply.github.com>
2022-09-12 19:37:23 -0500

    rerearranged function - added unique test

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -10,14 +10,14 @@ def add_integer(a, b=98):
     this function returns the sum of two numbers
     """
 
-    if a is None:
-        raise TypeError("'NoneType' object is not subscriptable")
-    if b is None:
-        raise TypeError("'NoneType' object is not subscriptable")
-
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
+
+    if a is None:
+        raise TypeError("'NoneType' object is not subscriptable")
+    if b is None:
+        raise TypeError("'NoneType' object is not subscriptable")
 
     return int(a) + int(b)
