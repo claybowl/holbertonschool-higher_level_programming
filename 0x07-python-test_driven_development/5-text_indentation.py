18f2d244cd63fa37d62eeb44ac5feaac2acbc5b4
Clayton Christian <103296303+claybowl@users.noreply.github.com>
2022-09-12 22:04:04 -0500

    fixed typo in function

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -14,7 +14,7 @@ def text_indentation(text):
     if type(text) is not str:
         raise TypeError("text must be a string")
 
-    for delim in ".?":
+    for delim in ".?:":
         text = (delim + "\n\n").join(
             [line.strip(" ") for line in text.split(delim)])
 
