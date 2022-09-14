711daae3cc90be01367e3b1279e1f4c9d3996592
Clayton Christian <103296303+claybowl@users.noreply.github.com>
2022-09-13 19:48:47 -0500

    added property/setter

--- 0x06-python-more_classes/0-rectangle.py
+++ 0x06-python-more_classes/0-rectangle.py
@@ -2,6 +2,9 @@
 """Writing a class that defines a rectangle by width and height"""
 
 
+from xml.etree.ElementPath import prepare_parent
+
+
 class Rectangle():
 	def __init__(self, width=0, height=0):
 		"""Initialize weight and height attributes"""
@@ -12,6 +15,25 @@ class Rectangle():
 			raise TypeError("width must be an integer")
 		if height is not type(int):
 			raise TypeError("height must be an integer")
-		if width <= 0:
+		if width < 0:
 			raise ValueError("width must be >= 0")
-	pass
+	
+	@property
+	def width(self):
+		"""Retrieves the width of a Rectangle instance"""
+		return self.__width
+	
+	@width.setter
+	def width(self, value):
+		"""Sets the width of a rectangle instance"""
+		self.__width = value
+
+	@property
+	def height(self, value):
+		"""Retrieves the height of Rectangle instance"""
+		return self.__height
+	
+	@height.setter
+	def height(self, value):
+		"""Sets the height of a Rectangle instance"""
+		self.__height = value
