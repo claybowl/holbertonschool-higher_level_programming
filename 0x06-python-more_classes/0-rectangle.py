8b4310b58b304214621bd54f5f0201e1e0f70799
Clayton Christian <103296303+claybowl@users.noreply.github.com>
2022-09-13 20:21:26 -0500

    style

--- 0x06-python-more_classes/0-rectangle.py
+++ 0x06-python-more_classes/0-rectangle.py
@@ -6,23 +6,23 @@ rectangle by width and height
 
 class Rectangle():
     """Rectangle class defined by width and height.
-	"""
+    """
     def __init__(self, width=0, height=0):
         """Initialize weight and height attributes
-		"""
+        """
         self.width = width
         self.height = height
 
     @property
     def width(self):
         """Retrieves the width of a Rectangle instance
-		"""
+        """
         return self.__width
 
     @width.setter
     def width(self, value):
         """Sets the width of a rectangle instance
-		"""
+        """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -32,13 +32,13 @@ class Rectangle():
     @property
     def height(self, value):
         """Retrieves the height of Rectangle instance
-		"""
+        """
         return self.__height
 
     @height.setter
     def height(self, value):
         """Sets the height of a Rectangle instance
-		"""
+        """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
