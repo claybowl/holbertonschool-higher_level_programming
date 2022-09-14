38eeae1e727831635de74162a71cd7570f341421
Clayton Christian <103296303+claybowl@users.noreply.github.com>
2022-09-13 21:23:52 -0500

    fixed typo

--- 0x06-python-more_classes/2-rectangle.py
+++ 0x06-python-more_classes/2-rectangle.py
@@ -12,59 +12,6 @@ class Rectangle:
         self.width = width
         self.height = height
 
-    @property
-    def width(self):
-        """Retrieves the width of a Rectangle instance"""
-        return self.__width
-
-    @property
-    def height(self):
-        """Retrieves the height of Rectangle instance"""
-        return self.__height
-
-    @width.setter
-    def width(self, value):
-        """Sets the width of a rectangle instance"""
-        if type(value) is not int:
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
-
-    @height.setter
-    def height(self, value):
-        """Sets the height of a Rectangle instance"""
-        if type(value) is not int:
-            raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = value
-
-    def area(self):
-        """Calculates the area of the rectangle"""
-        return self.__width * self.__height
-
-    def perimeter(self):
-        """Calculates the perimeter"""
-        if self.__height == 0 or self.__width == 0:
-            return 0
-        return 2 * (self.__width + self.__height)
-
-
-#!/usr/bin/python3
-"""Writing a class that defines a
-rectangle by width and height
-"""
-
-
-class Rectangle:
-    """Rectangle class defined by width and height"""
-
-    def __init__(self, width=0, height=0):
-        """Initialize weight and height attributes"""
-        self.width = width
-        self.height = height
-
     def __str__(self):
         """Returns an informal and nicely printable string representation
         of a Rectangle instance, filled with the '#' character."""
