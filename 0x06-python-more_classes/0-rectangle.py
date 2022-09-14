95084d3a2b54d699df11b8e0dc5151f8e39f1fd6
Clayton Christian <103296303+claybowl@users.noreply.github.com>
2022-09-13 19:58:23 -0500

    style commit

--- 0x06-python-more_classes/0-rectangle.py
+++ 0x06-python-more_classes/0-rectangle.py
@@ -6,18 +6,20 @@ rectangle by width and height
 
 class Rectangle():
 	"""Rectangle class defined by width and height."""
-	
+
 	def __init__(self, width=0, height=0):
 		"""Initialize weight and height attributes"""
 		self.width = width
 		self.height = height
 
-		if width is not type(int):
+		if not isinstance(width, int):
 			raise TypeError("width must be an integer")
-		if height is not type(int):
+		if not isinstance(height, int):
 			raise TypeError("height must be an integer")
 		if width < 0:
 			raise ValueError("width must be >= 0")
+		if height < 0:
+			raise ValueError("height must be >= 0")
 	
 	@property
 	def width(self):
