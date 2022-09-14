dc40b0fb7f123801f557b51b33eca480542d47d6
Clayton Christian <103296303+claybowl@users.noreply.github.com>
2022-09-13 20:29:41 -0500

    style

--- 0x06-python-more_classes/0-rectangle.py
+++ 0x06-python-more_classes/0-rectangle.py
@@ -10,14 +10,20 @@ class Rectangle():
     def __init__(self, width=0, height=0):
         """Initialize weight and height attributes
         """
-        self.width = width
         self.height = height
+        self.width = width
 
     @property
     def width(self):
         """Retrieves the width of a Rectangle instance
         """
         return self.__width
+
+    @property
+    def height(self, value):
+        """Retrieves the height of Rectangle instance
+        """
+        return self.__height
 
     @width.setter
     def width(self, value):
@@ -28,12 +34,6 @@ class Rectangle():
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-
-    @property
-    def height(self, value):
-        """Retrieves the height of Rectangle instance
-        """
-        return self.__height
 
     @height.setter
     def height(self, value):
