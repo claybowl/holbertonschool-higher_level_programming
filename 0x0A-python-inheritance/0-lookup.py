71c291875aafd87da4d7237849769efbf3cdc671
Clayton Christian <103296303+claybowl@users.noreply.github.com>
2022-09-15 17:16:09 -0500

    directory creation/task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Function that returns the list of available attributes and methods"""
+
+
+def lookup(obj):
+    """"Returns list of attributes and methods"""
+
+    return dir(obj)
