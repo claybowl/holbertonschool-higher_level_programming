0227b7ef4abd0a705cf939aa658f7644b06060d8
Clayton Christian <103296303+claybowl@users.noreply.github.com>
2022-09-15 18:38:30 -0500

    task 4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module 4-inherits_from
+Returns True if obj is an instance of parent class
+otherwise False
+"""
+
+
+def inherits_from(obj, a_class):
+    """Tests if obj is instance of a_class parent class"""
+
+    return isinstance(obj, a_class) and type(obj) != a_class
