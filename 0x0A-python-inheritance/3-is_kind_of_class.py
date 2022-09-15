eed8a1c2c4ecb370c03531aa415b5d89df2f3176
Clayton Christian <103296303+claybowl@users.noreply.github.com>
2022-09-15 18:32:26 -0500

    style

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -7,6 +7,7 @@ or parent class; otherwise False
 
 def is_kind_of_class(obj, a_class):
     """Discovers if obj is instance of a_class or its parent class"""
+
     pass
 
     return True if type(obj) is a_class or super(a_class) else False
