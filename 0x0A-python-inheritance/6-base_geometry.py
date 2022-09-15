5aa345521303837a80921609831836e4472d5838
Clayton Christian <103296303+claybowl@users.noreply.github.com>
2022-09-15 18:51:03 -0500

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module 5-base_geometry
+Creating an empty class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """Empty Class"""
+
+    def area(self):
+        """Area"""
+
+    raise Exception('area() is not implemented')
