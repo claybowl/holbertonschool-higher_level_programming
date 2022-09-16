a67085755855651095c3eda690bfccbc8663411e
Clayton Christian <103296303+claybowl@users.noreply.github.com>
2022-09-15 20:46:31 -0500

    task 11 attempt

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Creates class Square which is 
+sub-class of Rectangle
+"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square Class"""
+
+    def __init__(self, size):
+        """Initializing size"""
+
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        """Returns string of super class"""
+
+        return super().__str__()
+
+    def area(self):
+        """area of Square"""
+
+        return self.__size ** 2
