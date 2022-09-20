24cec91f5dfc614ed74a3cdd6c01425934b8b2d6
Clayton Christian <103296303+claybowl@users.noreply.github.com>
2022-09-19 23:12:23 -0500

    task 8 style update

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -4,6 +4,7 @@ Returns the dictionary description with simple data structure for JSON
 serialization of an object
 """
 
+
 def class_to_json(obj):
     """creates a dictionary description of object"""
 
