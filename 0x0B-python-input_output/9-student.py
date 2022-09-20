bb36c0facba6156ea3fdc8923e3f79b0d6e20786
Clayton Christian <103296303+claybowl@users.noreply.github.com>
2022-09-19 23:26:33 -0500

    task 9 update

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -10,7 +10,9 @@ class Student:
     def __init__(self, first_name, last_name, age):
         """Initializes student attributes"""
 
-        pass
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
     def to_json(self):
         """retrieves dictionary representation of student instance"""
