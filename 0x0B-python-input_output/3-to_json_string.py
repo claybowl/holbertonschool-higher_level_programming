e8d71076f3f92b1eaca09f2f2c27c2f4cacb48f1
Clayton Christian <103296303+claybowl@users.noreply.github.com>
2022-09-19 21:52:24 -0500

    task 3 attempt

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module 3-to_json_string.py
+Returns JSON representation of an object(string)
+"""
+
+
+def to_json_string(my_obj):
+    """returns the JSON representation of object"""
+
+    return json.dumps(my_obj)
