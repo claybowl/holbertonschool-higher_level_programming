6868ed66cae706df2805d14fa4947b0cac3d696c
Clayton Christian <103296303+claybowl@users.noreply.github.com>
2022-09-19 22:09:41 -0500

    task 4

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module 4-from_json_string
+Returns an object (python data structure) represented by a JSON string
+"""
+
+
+import json
+
+
+def from_json_string(my_obj):
+    """Returns an object represented by a JSON string"""
+
+    return json.loads(my_str)
