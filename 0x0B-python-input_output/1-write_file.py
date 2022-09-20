3cbee0b36c5efc5d6813bb67d19939ec8d4e5f4e
Clayton Christian <103296303+claybowl@users.noreply.github.com>
2022-09-19 21:16:12 -0500

    task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -8,5 +8,5 @@ returns number of characters written
 def write_file(filename="", text=""):
     """writes a string to text file and returns number of characters"""
 
-    with open(filename, 'r+') as f:
+    with open(filename, 'w+') as f:
         return f.write(text)
