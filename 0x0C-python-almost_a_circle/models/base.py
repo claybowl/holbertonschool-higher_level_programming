8c4261d254bea06267221ac18286a96de0c1ebd0
Clayton Christian <103296303+claybowl@users.noreply.github.com>
2022-09-23 14:27:04 -0500

    load_from_file rework

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -67,16 +67,16 @@ class Base:
         """Returns a list of object instances created fromjson
             file <class>.json"""
 
-        filename = cls.__name__ + ".json"
-        l = []
-        list_dicts = []
-        if os.path.exists(filename):
-            with open(filename, 'r') as f:
-                s = f.read()
-                list_dicts = cls.from_json_string(s)
-                for d in list_dicts:
-                    l.append(cls.create(**d))
-        return l
+        try:
+            filename = cls.__name__+".json"
+            with open(filename, "r", encoding="UTF8") as openedFile:
+                file_string = openedFile.read()
+                dlist = []
+                for obj in cls.from_json_string(file_string):
+                    dlist.append(cls.create(**obj))
+        except:
+            dlist = []
+        return dlist
 
     def integer_validator(self, value, num):
         """validate that num is an integer"""
