365a9244e3007ea1e22ba53749c2dafc268ee7c9
Clayton Christian <103296303+claybowl@users.noreply.github.com>
2022-09-22 20:16:05 -0500

    fixed style

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -23,23 +23,23 @@ class Base:
             self.id = Base.__nb_objects
 
 
-    def integer_validator(value, num):
-        """validate that num is an integer"""
+def integer_validator(value, num):
+    """validate that num is an integer"""
 
-        if type(value) is not str:
-            raise TypeError("value passed to validate_int must be string")
-        if type(num) is not int:
-            raise TypeError(f"{value} must be an integer")
-        if num <= 0:
-            raise ValueError(f"{value} must be > 0")
+    if type(value) is not str:
+        raise TypeError("value passed to validate_int must be string")
+    if type(num) is not int:
+        raise TypeError(f"{value} must be an integer")
+    if num <= 0:
+        raise ValueError(f"{value} must be > 0")
 
 
-    def no_neg_validator(value, num):
-        """validate that num is a 0 or positive integer"""
+def no_neg_validator(value, num):
+    """validate that num is a 0 or positive integer"""
 
-        if type(value) is not str:
-            raise TypeError("value passed to validate_not_neg must be string")
-        if type(num) is not int:
-            raise TypeError(f"{value} must be an integer")
-        if num < 0:
-            raise ValueError(f"{value} must be >= 0")
+    if type(value) is not str:
+        raise TypeError("value passed to validate_not_neg must be string")
+    if type(num) is not int:
+        raise TypeError(f"{value} must be an integer")
+    if num < 0:
+        raise ValueError(f"{value} must be >= 0")
