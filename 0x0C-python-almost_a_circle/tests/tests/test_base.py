f9ba1b4a00764b80b3a2a07760ed08c28e21eb16
Clayton Christian <103296303+claybowl@users.noreply.github.com>
2022-09-23 19:08:35 -0500

    test files revert

--- 0x0C-python-almost_a_circle/tests/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/tests/test_base.py
@@ -1,31 +1,12 @@
 #!/usr/bin/python3
+"""Unittest for max_integer([..])
 """
-Test for Base Class
-"""
-import inspect
-from models.base import Base
-from models.rectangle import Rectangle
-from models.square import Square
+
 import unittest
-import pycodestyle
-import pep8
-import json
-import os
+max_integer = __import__('6-max_integer').max_integer
 
 
-class TestBaseClass(unittest.TestCase):
-    """
-    Test Base Class
-    """
 
-    def test_pep8_base(self):
-        """
-        Test that models/base.py is pep8 compliant.
-        """
-        pep8style = pep8.StyleGuide(quiet=True)
-        result = pep8style.check_files(['models/base.py'])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warnings).")
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
