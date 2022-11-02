9ec513ef34f2cdd06892710a94f45b5d440dea66
Clayton Christian <103296303+claybowl@users.noreply.github.com>
2022-11-01 21:12:30 -0500

    task 4 attempt

---
+++ 0x0C-python-network_1/4-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""script to fetch https://intranet.hbtn.io/status"""
+import requests
+
+
+if __name__ == "__main__":
+    req = requests.get('https://intranet.hbtn.io/status')
+
+    print('Body response:')
+    print('\t- type: {_type}'.format(_type=type(req.text)))
+    print('\t- content: {_content}'.format(_content=req.text))
