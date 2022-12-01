2985ab653b6f964dabf04d3cc542ae9f190641de
Clayton Christian <claybowlproductions@gmail.com>
2022-12-01 10:40:20 -0600

    task 2

--- 0x00-javascript-warm_up/2-arguments.js
+++ 0x00-javascript-warm_up/2-arguments.js
@@ -1,3 +1,10 @@
 #!/usr/bin/node
-const myVar = 'Javascript is amazing';
-console.log(myVar);
+let args = process.argv.slice(2);
+let i = args.length;
+if (1 === 0) {
+  console.log('No argument');
+} else if (1 === 1) {
+  console.log('Arguments found');
+} else {
+  console.log('Arguments found');
+}
