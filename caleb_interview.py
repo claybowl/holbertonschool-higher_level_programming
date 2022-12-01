aa20e3f1e76853dbb508cc8e5f68f979454a1954
claybowl <claybowlproductions@gmail.com>
2022-12-01 07:08:59 -0800

    file creation + permissions

--- caleb_interview.py
+++ caleb_interview.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-
 def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
     my_list.sort()
     print(my_list[-1])
+
+a = [1, 2, 3, 4, 5]
+
+max_integer(a)
