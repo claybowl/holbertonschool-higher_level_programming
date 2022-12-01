37176c7c92a2d0d2debd55f07f4edfb2ef4cb824
Clayton Christian <claybowlproductions@gmail.com>
2022-12-01 11:37:09 -0600

    style?

--- 0x00-javascript-warm_up/2-arguments.js
+++ 0x00-javascript-warm_up/2-arguments.js
@@ -3,7 +3,7 @@ let args = process.argv.slice(2);
 let n = args.length;
 if (n === 0) {
   console.log('No argument');
-    } else if (n === 1) {
+  } else if (n === 1) {
       console.log('Arguments found');
     } else {
     console.log('Arguments found');
