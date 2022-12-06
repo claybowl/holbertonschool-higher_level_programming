f7a206eb907179c1dfa7d3a94e0c0df47e1a77e3
Clayton Christian <claybowlproductions@gmail.com>
2022-12-06 17:35:46 -0600

    readme, task 0

---
+++ 0x02-javascript-web_scraping/0-readme.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const fs = require('fs');
+const fileName = process.argv.slice(2);
+fs.readFile(fileName[0], 'utf8', (err, data) => {
+  if (err) throw err; 
+
+  console.log(data); 
+});
