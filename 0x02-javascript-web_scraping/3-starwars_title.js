99a1d9b258c480e5ef296a7fa752936b44106b53
Clayton Christian <claybowlproductions@gmail.com>
2022-12-06 20:31:38 -0600

    task 3 semistyle

--- 0x02-javascript-web_scraping/3-starwars_title.js
+++ 0x02-javascript-web_scraping/3-starwars_title.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const request = require('request');
 const movieId = process.argv[2];
-const url = 'https://swapi-api.hbtn.io/api/films/' + movieId
-request(url, function(error, response, body) {
+const url = 'https://swapi-api.hbtn.io/api/films/' + movieId;
+request(url, function (error, response, body) {
   if (error) {
-    console.error(err);
+    console.error(error);
   } else {
     console.log(JSON.parse(body).title);
   }
