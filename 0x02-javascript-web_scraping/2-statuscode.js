4f63c9e39b230ad3db4a24bf65a1dfce70ee5ea1
Clayton Christian <claybowlproductions@gmail.com>
2022-12-06 19:26:13 -0600

    task 2 style update

--- 0x02-javascript-web_scraping/2-statuscode.js
+++ 0x02-javascript-web_scraping/2-statuscode.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 const https = require('https');
-// retrieves url for get request from command line argument
 const url = process.argv[2];
 https.get(url, res => {
-  console.log('code: ${res.statusCode}');
+  console.log(`code: ${res.statusCode}`);
 });
