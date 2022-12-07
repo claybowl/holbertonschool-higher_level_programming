ad447f5b09a028aea5f0d5e3f71ac12191ba31d4
Clayton Christian <claybowlproductions@gmail.com>
2022-12-06 22:31:56 -0600

    task 5 attempt

---
+++ 0x02-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const request = require('request');
+const fs = require('fs');
+
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+request.get(url, (err, res, body) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  fs.writeFile(filePath, body, err => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+  });
+});
