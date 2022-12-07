b137e7647cd09871366273949aa6b2924123e297
Clayton Christian <claybowlproductions@gmail.com>
2022-12-06 18:03:41 -0600

    task 1 attempt

---
+++ 0x02-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const fs = require('fs');
+const filename = process.argv[2];
+const str = process.argv[3];
+fs.writeFile(filename, str, 'utf8', err => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+});
