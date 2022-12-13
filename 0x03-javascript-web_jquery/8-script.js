062098e09f6e8f2bc3f54d64c14c6d7985c9a4ce
Clayton Christian <claybowlproductions@gmail.com>
2022-12-12 22:10:55 -0600

    task 5 - 9 attemtps

---
+++ 0x03-javascript-web_jquery/8-script.js
@@ -0,0 +1,7 @@
+$(function () {
+  $.get('https://swapi-api.hbtn.io/api/films/?format=json', function (data) {
+    $.each(data.results, function (i, film) {
+      $('#list_movies').append('<li>' + film.title + '</li>');
+    });
+  });
+});
