c74376730e1d0cbb9ec786ca050461f3a822588b
claybowl <claybowlproductions@gmail.com>
2023-03-14 05:15:40 -0500

    file creation/permissions/task 0 and 1 attempt

---
+++ 0x0C-python-network_1/1-hbtn_header.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""script takes & sends request to URL & displays value of X-Request-ID"""
+import sys
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    request = urllib.request.Request(url)
+    with urllib.request.urlopen(request) as response:
+        print(dict(response.headers).get("X-Request-Id"))
+2-post_email.py
